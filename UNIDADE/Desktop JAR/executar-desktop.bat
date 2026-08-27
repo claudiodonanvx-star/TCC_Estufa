cd4a9373c754ac39f2ad8734df4110a3ccfa6366
claudiodonanvx-estrela <claudiodona.nvx@gmail.com>
2026-08-27 12:06:34 -0300

    Revert_executar_desktop

--- UNIDADE/Desktop JAR/executar-desktop.bat
+++ UNIDADE/Desktop JAR/executar-desktop.bat
@@ -1,4 +1,24 @@
 @echo off
+REM Atalho para executar o desktop Java na pasta UNIDADE\desktop_java_unidade usando o Gradle wrapper
+pushd "%~dp0..\desktop_java_unidade" || (
+  echo Diretorio do projeto nao encontrado.
+  pause
+  exit /b 1
+)
+
+if not exist "gradlew.bat" (
+  echo gradlew.bat nao encontrado em %CD%.
+  popd
+  pause
+  exit /b 1
+)
+
+echo Iniciando aplicativo desktop...
+call gradlew.bat run
+popd
+pause
+
+@echo off
 REM Menu para utilitários Desktop - Estufa Smart
 setlocal enabledelayedexpansion
 :menu
@@ -54,3 +74,4 @@ goto menu
 :fim
 endlocal
 exit /b 0
+
